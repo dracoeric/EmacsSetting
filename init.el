993f8e82d087ef8db2ddab26bd20ca825d588fc4
Eric LI <dracoeric@FirstBorn.localdomain>
2018-11-05 21:09:02 +0100

    Parametre emacs v1

---
+++ init.el
@@ -0,0 +1,79 @@
+;*******************************************************************************;
+;                                                                               ;
+;                                                          :::      ::::::::    ;
+;    dotemacs                                            :+:      :+:    :+:    ;
+;                                                      +:+ +:+         +:+      ;
+;    by: thor <thor@42.fr>                           +#+  +:+       +#+         ;
+;                                                  +#+#+#+#+#+   +#+            ;
+;    Created: 2013/06/18 14:01:14 by thor               #+#    #+#              ;
+;    Updated: 2018/11/05 21:04:24 by erli             ###   ########.fr        ;
+;                                                                               ;
+;*******************************************************************************;
+
+; Load general features files
+(setq config_files "/usr/share/emacs/site-lisp/")
+(setq load-path (append (list nil config_files) load-path))
+
+(load "~/.emacs.d/list.el")
+(load "~/.emacs.d/string.el")
+(load "~/.emacs.d/comments.el")
+(load "~/.emacs.d/header.el")
+
+(autoload 'php-mode "php-mode" "Major mode for editing PHP code" t)
+(add-to-list 'auto-mode-alist '("\\.php[34]?\\'\\|\\.phtml\\'" . php-mode))
+
+; Set default emacs configuration
+(set-language-environment "UTF-8")
+(setq-default font-lock-global-modes t)
+(setq-default line-number-mode nil)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode t)
+(global-set-key (kbd "DEL") 'backward-delete-char)
+(setq-default c-backspace-function 'backward-delete-char)
+(setq-default c-basic-offset 4)
+(setq-default c-default-style "linux")
+(setq-default tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
+	  		  				 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
+
+; Load user configuration
+(if (file-exists-p "~/.myemacs") (load-file "~/.myemacs"))
+
+; Load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
+
+;*******************************************************************************;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   (quote
+	("a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" default)))
+ '(line-number-mode nil)
+ '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#32302f"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; Load theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/gruvbox-theme-20181013.1144")
+(add-hook 'after-init-hook (lambda () (load-theme 'gruvbox-dark-soft)))
+(package-initialize)
+(load-theme 'gruvbox-dark-soft)
+
+;; Syntaxe Highlighting
+(font-lock-mode t)
